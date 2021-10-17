69533cadf25f428db53a006bf95676aa0a74188e
nh-polash <polashhoque0@gmail.com>
2021-10-17 21:11:32 +0600

    some changes

--- src/component/Total/Total.js
+++ src/component/Total/Total.js
@@ -4,10 +4,10 @@ import './Total.css';
 const Total = (props) => {
     // console.log(props);
     const {addedMember} = props;
-    addedMember.reduce((previous, price) => previous + price.balance,0);
+    const total = addedMember.reduce((previous, price) => previous + price.balance,0);
     let totals = 0;
     for(const price of addedMember){
-        totals = totals + price.balance;
+        totals = total + price.balance;
     }
 
     
@@ -15,7 +15,7 @@ const Total = (props) => {
     return (
         <div>
             <h1>Member Added:{addedMember.length}</h1>
-            <h1>Total Balance:{totals} </h1>
+            <h1>Total Balance:{totals } </h1>
         </div>
     );
 };
